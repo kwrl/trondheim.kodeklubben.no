91f898bd9726d6c7c5d32d48e5c24f0a95521e44
= <haakongk@stud.ntnu.no>
2014-09-03 15:27:34 +0000

    Filter by field registration

--- backend/wsgi/courses/admin.py
+++ backend/wsgi/courses/admin.py
@@ -7,6 +7,7 @@ class CourseAdmin(admin.ModelAdmin):
 
 class RegistrationAdmin(admin.ModelAdmin):
     list_display = ('course','user','granted', 'code_master')
+    list_filter = ('course','user','granted','code_master')
 
 admin.site.register(Course,CourseAdmin)
 admin.site.register(Registration, RegistrationAdmin)
