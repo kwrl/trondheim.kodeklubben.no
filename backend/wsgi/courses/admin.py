879f0271caf12e4569874f309a0156f0a054fdaa
= <haakongk@stud.ntnu.no>
2014-09-03 15:22:18 +0000

    added code_master boolean to list view admin

--- backend/wsgi/courses/admin.py
+++ backend/wsgi/courses/admin.py
@@ -6,7 +6,7 @@ class CourseAdmin(admin.ModelAdmin):
     list_display = ('name','desc')
 
 class RegistrationAdmin(admin.ModelAdmin):
-    list_display = ('course','user','granted')
+    list_display = ('course','user','granted', 'code_master')
 
 admin.site.register(Course,CourseAdmin)
 admin.site.register(Registration, RegistrationAdmin)
