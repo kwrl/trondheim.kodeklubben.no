ef44e1eb09f0be30f8a2f09fc7a7c99bd8d2062c
kwrl <haakongk@stud.ntnu.no>
2015-02-15 23:07:35 +0100

    Using builtin auth views. Urls contained in usermanagement

--- backend/wsgi/kodeklubb/urls.py
+++ backend/wsgi/kodeklubb/urls.py
@@ -2,7 +2,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-
 from filebrowser.sites import site as browsersite
 
 admin.autodiscover()
@@ -14,24 +13,5 @@ urlpatterns = patterns('',
     url(r'^ckeditor/',include('ckeditor.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^courses/', include('courses.urls')),
-    url(r'^users/',include('usermanagement.urls')),
-    url(r'^accounts/login/$',
-        'django.contrib.auth.views.login',
-        {'template_name' : 'form_screen.html'},
-        name='login'),
-    url(r'^accounts/logout/$',
-        'django.contrib.auth.views.logout',
-        name='logout'),
-    url(r'^user/password/reset/$',
-        'django.contrib.auth.views.password_reset',
-        {'post_reset_redirect' : '/user/password/reset/done',
-        'template_name' : 'form_screen.html'},
-        name='password_reset'),
-        (r'^user/password/reset/done/$',
-        'django.contrib.auth.views.password_reset_done'),
-        (r'^user/password/reset/(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/$',
-        'django.contrib.auth.views.password_reset_confirm',
-        {'post_reset_redirect' : '/user/password/done'}),
-        (r'^user/password/done/$',
-        'django.contrib.auth.views.password_reset_complete')
+    url(r'^accounts/',include('usermanagement.urls')),
 ) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- backend/wsgi/usermanagement/urls.py
+++ backend/wsgi/usermanagement/urls.py
@@ -1,9 +1,34 @@
 from django.conf.urls import patterns, include, url
-from .views import EditUserView, UserLoginView, UserLogoutView, UserCreateView
+from django.views.generic.edit import CreateView
+from django.contrib.auth.forms import UserCreationForm
 
 urlpatterns = patterns('',
-        url(r'^register/$', UserCreateView.as_view(), name="register"),
-        #url(r'^login/$', UserLoginView.as_view(), name="login"),
-        #url(r'^logout/$', UserLogoutView.as_view(), name="logout"),
-        url(r'^edit_user/$', EditUserView.as_view(), name='edit_user')
+    url(r'^register/$',
+        CreateView.as_view(
+            template_name='form_screen.html',
+            form_class=UserCreationForm,
+            success_url='/'
+        ),
+        name='register'),
+    url(r'^login/$',
+        'django.contrib.auth.views.login',
+        {'template_name' : 'form_screen.html'},
+        name='login'),
+    url(r'^logout/$',
+        'django.contrib.auth.views.logout',
+        name='logout'),
+    url(r'^password/reset/$',
+        'django.contrib.auth.views.password_reset',
+        {'post_reset_redirect' : '/user/password/reset/done',
+        'template_name' : 'form_screen.html'},
+        name='password_reset'),
+    url(r'^password/reset/done/$',
+        'django.contrib.auth.views.password_reset_done'),
+    url(r'^user/password/reset/(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/$',
+        'django.contrib.auth.views.password_reset_confirm',
+        {'post_reset_redirect' : '/user/password/done'},
+        name='password_reset_done'),
+    url(r'^password/done/$',
+        'django.contrib.auth.views.password_reset_complete',
+        name='password_done')
 )
